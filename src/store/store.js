57e7411abf6fad1938d21ea959cdcf66cace607b
leathergreengargoyle <avtran23@gmail.com>
2017-07-17 22:33:26 -0700

    [progress]Create store

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from 'redux'
+import { createLogger } from 'redux-logger'
+import rootReducer from './reducers'
+
+const plugins = [thunk]
+
+if (process.env.NODE_ENV !== 'production') {
+  plugins.push(createLogger({}))
+}
+
+const middleware = applyMiddleware(...plugins)
+const store = createStore(rootReducer, middleware)
+
+export default store
