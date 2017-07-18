bd5f5382ae8702e46077d2d52feae5d30cf039d1
leathergreengargoyle <avtran23@gmail.com>
2017-07-17 22:25:29 -0700

    [progress]Create Constants file, posts reducer

---
+++ src/store/reducers.js
@@ -0,0 +1,26 @@
+import { combineReducers } from 'redux'
+import { data, actionTypes } from '../constants'
+
+const posts = (state = data, { type, payload }) => {
+  const newState = [...state]
+  switch (type) {
+ // payload will be {postIdx: num, like: bool}
+    case actionTypes.LIKE_PHOTO:
+      if (payload.like) {
+        newState[payload.postIdx].likes++
+      } else {
+        newState[payload.postIdx].likes--
+      }
+      return newState
+
+// payload will be {postIdx: num, commentObj: {id, name, comment}}
+    case actionTypes.POST_COMMENT:
+      newState[payload.postIdx].comments.push(payload.commentObj)
+      return newState
+
+    default:
+      return state
+  }
+}
+
+export default combineReducers({ posts })
