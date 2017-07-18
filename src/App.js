0a3f567d947d07ca7a73bb430dfe20d5d1905356
leathergreengargoyle <avtran23@gmail.com>
2017-07-17 21:36:32 -0700

    [progress]Refactor App to contain routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Feed from './components/Feed'
+import CommentsList from './components/CommentsList'
 import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Switch>
+      <Route exact path="/" component={Feed} />
+      <Route path="/comments" component={CommentsList} />
+    </Switch>
+  </div>
+)
 
-export default App;
+export default App
